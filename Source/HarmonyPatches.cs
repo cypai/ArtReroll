b1766c836d72ced01b05a8829229a59d318d6ad7
Cheng-Yu Pai <mail4charliepai@gmail.com>
2021-03-14 01:49:01 -0800

    Implemented simple art reroll button

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using HarmonyLib;
 using RimWorld;
 using Verse;
-using Verse.AI;
+using UnityEngine;
 
 namespace ArtReroll
 {
@@ -16,6 +14,32 @@ namespace ArtReroll
         static HarmonyPatches()
         {
             Harmony harmony = new Harmony("com.pipai.artreroll");
+
+            PatchPostfix(harmony, typeof(ITab_Art), "FillTab", "FillTab");
+        }
+
+        private static void PatchPostfix(Harmony harmony, Type type, string target, string postfix)
+        {
+            MethodInfo targetMethod = AccessTools.Method(type, target);
+            HarmonyMethod postfixMethod = new HarmonyMethod(typeof(HarmonyPatches).GetMethod(postfix));
+            harmony.Patch(targetMethod, null, postfixMethod);
+        }
+
+        public static void FillTab(ITab_Art __instance)
+        {
+            Rect rect = new Rect(10f, 270f, 100f, 20f);
+            if (Widgets.ButtonText(rect, "Reroll"))
+            {
+                Thing thing = Find.Selector.SingleSelectedThing;
+                if (thing is MinifiedThing minifiedThing)
+                {
+                    thing = minifiedThing.InnerThing;
+                }
+                CompArt art = thing?.TryGetComp<CompArt>();
+                TaleReference originalTale = art.TaleRef;
+                art.InitializeArt(ArtGenerationContext.Colony);
+                originalTale.ReferenceDestroyed();
+            }
         }
     }
 }
