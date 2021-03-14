aed5d67760ca8dfbe89eb28e05dc481e191caa21
Cheng-Yu Pai <mail4charliepai@gmail.com>
2021-03-14 00:53:14 -0800

    Initial project setup

---
+++ Source/HarmonyPatches.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace ArtReroll
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony("com.pipai.artreroll");
+        }
+    }
+}
